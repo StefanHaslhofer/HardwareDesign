28cab301c97c1e7f2b51fc5c5dd933dad828866b
StefanHaslhofer <haslhofer.stefan@gmail.com>
2024-10-26 10:27:22 +0200

    working blinky

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-yosys -p 'synth_ecp5 -json blinky.json' blinky.v              
+yosys -p "synth_ecp5 -json blinky.json" blinky.v              
 nextpnr-ecp5 --85k --json blinky.json --lpf ulx3s_v20.lpf --textcfg ulx3s_out.config --package CABGA381
 ecppack ulx3s_out.config ulx3s.bit
 fujprog ulx3s.bit
