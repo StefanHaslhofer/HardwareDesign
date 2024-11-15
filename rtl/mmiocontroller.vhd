97af018b8624fa267ae541354dd77319792ae574
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2024-11-15 14:05:02 +0100

    rename data memory wires

--- rtl/mmiocontroller.vhd
+++ rtl/mmiocontroller.vhd
@@ -5,8 +5,8 @@ use IEEE.NUMERIC_STD_UNSIGNED.all;
 
 entity mmiocontroller is
   port(clk_25mhz: in  STD_ULOGIC;
-       wesp:  in  STD_ULOGIC_VECTOR(7 downto 0);
-       resp:  out STD_ULOGIC_VECTOR(7 downto 0));
+       wioc:  in  STD_ULOGIC_VECTOR(7 downto 0);
+       rioc:  out STD_ULOGIC_VECTOR(7 downto 0));
 end;
 
 architecture behave of mmiocontroller is
