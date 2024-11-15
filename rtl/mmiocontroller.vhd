1c0a18713dd0a3f29585539fbebf7a9991eab709
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2024-11-15 14:01:03 +0100

    connect mmiocontroller and core

--- rtl/mmiocontroller.vhd
+++ rtl/mmiocontroller.vhd
@@ -3,21 +3,16 @@ use IEEE.STD_LOGIC_1164.all;
 use STD.TEXTIO.all;
 use IEEE.NUMERIC_STD_UNSIGNED.all;
 
-entity mmiocontroler is -- data memory
-  generic(addr_width: integer := 8);
+entity mmiocontroller is
   port(clk_25mhz: in  STD_ULOGIC;
-       we:  in  STD_ULOGIC;
-       sel: in  STD_ULOGIC_VECTOR(1 downto 0);
-       a:   in  STD_ULOGIC_VECTOR(31 downto 0);
-       wd:  in  STD_ULOGIC_VECTOR(31 downto 0);
-       rd:  out STD_ULOGIC_VECTOR(31 downto 0);
-       -- read vector from esp32
-       resp:  out STD_ULOGIC_VECTOR(7 downto 0);
-       -- write vector to esp32
-       wesp:  out STD_ULOGIC_VECTOR(7 downto 0));
+       wesp:  in  STD_ULOGIC_VECTOR(7 downto 0);
+       resp:  out STD_ULOGIC_VECTOR(7 downto 0));
 end;
 
-architecture behave of dmem is
+architecture behave of mmiocontroller is
 begin
+  process (clk_25mhz, wesp) is
+  begin
 
+  end process
 end;
