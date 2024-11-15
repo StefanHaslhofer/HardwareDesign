6cc00cb547b9fe8428739a303cdd74fe2bea40ec
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2024-11-15 14:51:25 +0100

    add gp pins as input output

---
+++ rtl/dmem.vhd
@@ -0,0 +1,64 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use STD.TEXTIO.all;
+use IEEE.NUMERIC_STD_UNSIGNED.all;
+
+entity dmem is -- data memory
+  generic(addr_width: integer := 8);
+  port(clk_25mhz: in  STD_ULOGIC;
+       we:  in  STD_ULOGIC;
+       sel: in  STD_ULOGIC_VECTOR(1 downto 0);
+       a:   in  STD_ULOGIC_VECTOR(31 downto 0);
+       wd:  in  STD_ULOGIC_VECTOR(31 downto 0);
+       rd:  out STD_ULOGIC_VECTOR(31 downto 0);
+       rioc:  in STD_ULOGIC_VECTOR(7 downto 0);
+       wioc:  out STD_ULOGIC_VECTOR(7 downto 0));
+end;
+
+architecture behave of dmem is
+begin
+  process (clk_25mhz, we, sel, wd, rioc) is
+    -- 256 byte
+    type ramtype is array (2**(addr_width)-1 downto 0) of STD_ULOGIC_VECTOR(7 downto 0);
+    variable dmem_s: ramtype := (others => (others => '0'));
+  begin
+    -- read or write memory
+      if rising_edge(clk_25mhz) then
+        if (we='1') then
+          if (sel="00") then  -- 8 bit data width
+            dmem_s(to_integer(a(addr_width-1 downto 0))) := wd(7 downto 0);
+          elsif (sel="01") then -- 16 bit data width
+            dmem_s(to_integer(a(addr_width-1 downto 0)))   := wd(7 downto 0);
+            dmem_s(to_integer(a(addr_width-1 downto 0))+1) := wd(15 downto 8);
+            -- assert (a(0) = '0') report "memory address alignment missmatch for half word access" severity error;
+          else   -- 32 bit data width
+            dmem_s(to_integer(a(addr_width-1 downto 0)))   := wd(7 downto 0);
+            dmem_s(to_integer(a(addr_width-1 downto 0))+1) := wd(15 downto 8);
+            dmem_s(to_integer(a(addr_width-1 downto 0))+2) := wd(23 downto 16);
+            dmem_s(to_integer(a(addr_width-1 downto 0))+3) := wd(31 downto 24);
+            -- assert (a(1 downto 0) = "00") report "memory address alignment missmatch for word access" severity error;
+          end if;
+        end if;
+
+        -- read 8 bit of data from mmio controller and store it at certain position
+        dmem_s(255) := rioc(7 downto 0);
+      end if;
+      rd <= (others => '0');
+      if (sel="00") then  -- 8 bit data width
+        rd(7 downto 0)   <= dmem_s(to_integer(a(addr_width-1 downto 0)));
+      elsif (sel="01") then -- 16 bit data width
+        rd(7 downto 0)   <= dmem_s(to_integer(a(addr_width-1 downto 0)));
+        rd(15 downto 8)  <= dmem_s(to_integer(a(addr_width-1 downto 0))+1);
+      else
+        rd(7 downto 0)   <= dmem_s(to_integer(a(addr_width-1 downto 0)));
+        rd(15 downto 8)  <= dmem_s(to_integer(a(addr_width-1 downto 0))+1);
+        rd(23 downto 16) <= dmem_s(to_integer(a(addr_width-1 downto 0))+2);
+        rd(31 downto 24) <= dmem_s(to_integer(a(addr_width-1 downto 0))+3);
+      end if;
+
+      -- read 8 bit of data and write to mmio controller if stored at certain position
+      if (to_integer(a(addr_width-1 downto 0))=254) then
+        wioc(7 downto 0) <= dmem_s(to_integer(a(addr_width-1 downto 0)));
+      end if;
+  end process;
+end;
---
+++ rtl/top.vhd
@@ -0,0 +1,72 @@
+-----------------------------------------
+-- Institute for Complex Systems (ICS)
+-- ICS RV32I v1.0.0
+-- based on Harris&Harris Implementation (see below **)
+-- daniel.grosse@jku.at
+-- lucas.klemmer@jku.at
+-- thanks to Richard Feichtinger for his work on this core
+--
+-----------------------
+-- **
+-- RISC-V single-cycle processor
+-- From Section 7.6 of Digital Design & Computer Architecture
+-- 27 April 2020
+-- David_Harris@hmc.edu 
+-- Sarah.Harris@unlv.edu
+
+
+Library IEEE;
+Library work;
+use IEEE.STD_LOGIC_1164.all;
+
+entity top is -- top-level design for testing
+  port(clk_25mhz: in     STD_ULOGIC;
+       reset:     in     STD_ULOGIC;
+       WriteData: buffer STD_ULOGIC_VECTOR(31 downto 0);
+       DataAdr:   buffer STD_ULOGIC_VECTOR(31 downto 0);
+       MemWrite:  buffer STD_ULOGIC;
+       gp, gn: inout std_logic_vector(27 downto 0));
+end;
+
+architecture test of top is
+  component riscvpipeline
+    port(clk_25mhz: in  STD_ULOGIC;
+         reset:     in  STD_ULOGIC;
+         PC:        out STD_ULOGIC_VECTOR(31 downto 0);
+         Instr:     in  STD_ULOGIC_VECTOR(31 downto 0);
+         MemWrite:  out STD_ULOGIC;
+         ALUResult: out STD_ULOGIC_VECTOR(31 downto 0);
+         MemSel:    out STD_ULOGIC_VECTOR(1  downto 0);
+         WriteData: out STD_ULOGIC_VECTOR(31 downto 0);
+         ReadData:  in  STD_ULOGIC_VECTOR(31 downto 0));
+  end component;
+  
+  component imem
+    port(a:  in  STD_ULOGIC_VECTOR(31 downto 0);
+         rd: out STD_ULOGIC_VECTOR(31 downto 0));
+  end component;
+  
+  component dmem
+    port(clk_25mhz: in  STD_ULOGIC;
+         we:  in  STD_ULOGIC;
+         sel: in  STD_ULOGIC_VECTOR(1 downto 0);
+         a:   in  STD_ULOGIC_VECTOR(31 downto 0);
+         wd:  in  STD_ULOGIC_VECTOR(31 downto 0);
+         rd:  out STD_ULOGIC_VECTOR(31 downto 0);
+         rioc:  in STD_ULOGIC_VECTOR(7 downto 0);
+         wioc:  out STD_ULOGIC_VECTOR(7 downto 0));
+  end component;
+    
+  signal PC:        STD_ULOGIC_VECTOR(31 downto 0);
+  signal Instr:     STD_ULOGIC_VECTOR(31 downto 0);
+  signal ReadData:  STD_ULOGIC_VECTOR(31 downto 0);
+  signal MemSel:    STD_ULOGIC_VECTOR(1  downto 0);
+  
+begin
+  -- instantiate processor and memories
+
+  rvsingle: entity work.riscvpipeline(struct_neg) port map(clk_25mhz, reset, PC, Instr, MemWrite, DataAdr, MemSel, WriteData, ReadData);
+  --rvsingle: riscvpipeline port map(clk, reset, PC, Instr, MemWrite, DataAdr, MemSel, WriteData, ReadData);
+  imem1: imem port map(PC, Instr);
+  dmem1: dmem port map(clk_25mhz, MemWrite, MemSel, DataAdr, WriteData, ReadData, gp(11));
+end;
