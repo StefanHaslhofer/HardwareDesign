375ba1389e57924e9ab03a40a591681104661e67
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2024-11-30 21:03:42 +0100

    working esp32 sketch

--- blinky.v
+++ blinky.v
@@ -1,14 +1,14 @@
 module top(input clk_25mhz,
            inout [27:0] gp,
+           inout [27:0] gn,
            output [7:0] led);
 
     wire rioc;
-    assign rioc= gp[12];
-    assign gp[13] = 1;
+    assign gp[12] = 1;
 
     always @(posedge clk_25mhz) begin
           led[6:0] <= 0;
-          led[7] <= rioc;
+          led[7] <= gn[12];
     end
 
 endmodule
--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,6 +1,7 @@
-#define INPUT_PIN 35 // GP13 on ULX3S <-> GPIO35 on ESP32
-#define OUTPUT_PIN 26 // GP11 on ULX3S <-> GPIO26 on ESP32
-#define OUTPUT_PIN_2 33 // GP12 on ULX3S <-> GPIO33 on ESP32
+// #define INPUT_PIN 35 // GP13 on ULX3S <-> GPIO35 on ESP32
+// #define OUTPUT_PIN 26 // GP11 on ULX3S <-> GPIO26 on ESP32
+#define OUTPUT_PIN 32 // GN12 on ULX3S <-> GPIO32 on ESP32
+#define INPUT_PIN 33 // GP12 on ULX3S <-> GPIO33 on ESP32
 
 void setup() {
   Serial.begin(115200);
@@ -8,7 +9,6 @@ void setup() {
   delay(10000);
   pinMode(INPUT_PIN, INPUT_PULLUP);
   pinMode(OUTPUT_PIN, OUTPUT);
-  pinMode(OUTPUT_PIN_2, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -18,11 +18,9 @@ void loop() {
   if (pinState == HIGH) {
     Serial.println("HIGH");
     digitalWrite(OUTPUT_PIN, HIGH);
-    digitalWrite(OUTPUT_PIN_2, HIGH);
   } else {
     Serial.println("LOW");
     digitalWrite(OUTPUT_PIN, LOW);
-    digitalWrite(OUTPUT_PIN_2, LOW); 
   }
 
   digitalWrite(LED_BUILTIN, HIGH);
