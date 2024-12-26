7b2da823275d178ca13148dee5d251d1ddce2de6
Stefan Haslhofer <haslhofer.stefan@gmail.com>
2024-12-26 11:23:40 +0100

    working c counter

--- LedCounter/main.c
+++ LedCounter/main.c
@@ -1,7 +1,17 @@
 int main(void) {
     int *ptr = (int *)255;
 
-    *ptr = 33;
+    for (int i = 0; i <= 255; i++) {
+        *ptr = i;
+
+        for (int j = 0; j <= 2500000; j++) {
+            *ptr = i;
+        }
+
+        if (i == 255) {
+            i = 0;
+        }
+    }
 
     return 0;
 }
--- LedCounter/main.s
+++ LedCounter/main.s
@@ -14,10 +14,39 @@ main:
 	addi	s0,sp,32
 .LCFI2:
 	li	a5,255
-	sw	a5,-20(s0)
-	lw	a5,-20(s0)
-	li	a4,33
+	sw	a5,-28(s0)
+	sw	zero,-20(s0)
+	j	.L2
+.L6:
+	lw	a5,-28(s0)
+	lw	a4,-20(s0)
 	sw	a4,0(a5)
+	sw	zero,-24(s0)
+	j	.L3
+.L4:
+	lw	a5,-28(s0)
+	lw	a4,-20(s0)
+	sw	a4,0(a5)
+	lw	a5,-24(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+.L3:
+	lw	a4,-24(s0)
+	li	a5,2498560
+	addi	a5,a5,1440
+	ble	a4,a5,.L4
+	lw	a4,-20(s0)
+	li	a5,255
+	bne	a4,a5,.L5
+	sw	zero,-20(s0)
+.L5:
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L2:
+	lw	a4,-20(s0)
+	li	a5,255
+	ble	a4,a5,.L6
 	li	a5,0
 	mv	a0,a5
 	lw	ra,28(sp)
